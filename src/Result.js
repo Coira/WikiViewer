c01477daf2798e884bfb9844568cc23ad867ebca
Coira <coira.ells@gmail.com>
2016-02-15 22:56:40 +0000

    Search working. Styling on the search bar

--- src/Result.js
+++ src/Result.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 class Result extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+    
     render() {
-	return (<div> This is a Result </div>);
+	return (
+	    <div>
+		<a href={this.props.url}>{this.props.title}</a>
+		{this.props.description}
+	    </div>);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import ReactDOM from 'react-dom';
 import Result from './Result';
 
 class Search extends React.Component {
+    
+    constructor(props) {
+	super(props);
+	this.state = { value: "Richard",
+		       results: [] };
+	this.handleChange = this.handleChange.bind(this);
+	this.search = this.search.bind(this);
+    }
+    
+    
     search() {
 	var searchResult = $.ajax({
 	    data: {
-		action: 'query',
-		list: 'search',
-		srsearch: 'Richard Feynman',
-		format: 'json' },
+		action: 'opensearch',
+		search: this.state.value,
+		format: 'json',
+		limit: 5
+	    },
 	    dataType: 'jsonp',
 	    jsonp: 'callback',
 	    url: 'https://en.wikipedia.org/w/api.php',
@@ -19,22 +30,65 @@ class Search extends React.Component {
 		console.log("error" + data.status);
 	    },
 	    success: function(data) {
-		console.log(data);
-	    }
+		let results = [];
+		
+		data[1].map((title, index) => {
+		    let result = {};
+		    result.title = title;
+		    result.description = data[2][index];
+		    result.url = data[3][index];
+		    results.push(result);
+		});
+		
+		this.setState({results});
+		
+	    }.bind(this)
 	});
     }
- 
+
+    handleChange(event) {
+	this.setState({value: event.target.value});
+    }
+
     render() {
 	return (
-	    <div> This is Search
-		<button onClick={this.search}>Click me</button>
+
+	    <div>
+		<div className="searchOuter">
+		    
+		    <input className="searchBox"
+			   type="text"
+			   value={this.state.value}
+			   onChange={this.handleChange} />
+		    
+		    <div className="searchButtonOuter">
+			<button className="searchButton"
+				onClick={this.search}>
+			    <i className="fa fa-search"></i>
+			</button>
+		    </div>
+		    
+		</div>
+
+    		{
+		    this.state.results.map((result, index) => {
+			return (
+			    <Result key={index}
+				    url={result.url}
+				    title={result.title}
+				    description={result.description}>
+			    </Result>
+			);
+		    })
+		}
+
 	    </div>
 	);
     }
 }
 
 ReactDOM.render(
-    <Search />,
-    document.getElementById('content')
+    <Search/>,
+    document.getElementById('app')
 );
-    
+
